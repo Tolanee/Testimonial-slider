9374bf8d2f9e7de3abbdcd3bf9c76e302fa3a1da
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-06-30 12:25:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const containerOne = document.getElementById('container-one');
+const containerTwo = document.getElementById('container-two');
+const previousBtn = document.getElementsByClassName('prev');
+const nextBtn = document.getElementsByClassName('next');
