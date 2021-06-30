85e59d263b73869d3d51edc650e1162e7992039b
Tolanee <68304958+Tolanee@users.noreply.github.com>
2021-06-30 19:58:32 +0100

    commit

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const containerOne = document.getElementById('container-one');
-const containerTwo = document.getElementById('container-two');
-const previousBtn = document.getElementsByClassName('prev');
-const nextBtn = document.getElementsByClassName('next');
+const containerOne = document.getElementById("container-one");
+const containerTwo = document.getElementById("container-two");
+const previousBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+
+previousBtn.addEventListener('click', previousPage)
+nextBtn.addEventListener('click', nextPage);
+
+containerOne.style.display = "block";
+containerTwo.style.display = "none";
+
+function previousPage(){
+  if (containerTwo.style.display === "block") {
+    containerOne.style.display = "none";
+  } else {
+    containerTwo.style.display = "block";
+    containerOne.style.display = "none"
+}
+}
+
+function nextPage(){
+  if (containerOne.style.display === "none") {
+    containerTwo.style.display = "block";
+  } else {
+    containerOne.style.display = "none";
+    containerTwo.style.display = "block"
+  }
+}
